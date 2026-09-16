6d2a3d6ac744ecccd94f20228b8438d8b29eb101
jycouet <jycouet@gmail.com>
2026-09-16 12:41:25 +0200

    tmp: branch marker on home page

--- template-postgres/src/routes/+page.svelte
+++ template-postgres/src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>TMP: deployed from branch fix-template</p>
+<p><a href="/demo/better-auth">better-auth demo</a></p>
