7c171f9d302e26835cecf9f6c6b81d7840262f7f
jycouet <jycouet@gmail.com>
2026-09-16 12:59:43 +0200

    drop branch marker

--- template-postgres/src/routes/+page.svelte
+++ template-postgres/src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>TMP: deployed from branch fix-template</p>
 <p><a href="/demo/better-auth">better-auth demo</a></p>
